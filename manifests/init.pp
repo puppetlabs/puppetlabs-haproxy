e229309a7b670278fb668d011ba191169b4918c5
Micah Anderson <micah@riseup.net>
2013-02-25 12:25:53 -0500

    add Debian to supported Operating systems (I have tested this)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # == Class: haproxy
 #
 # A Puppet module, using storeconfigs, to model an haproxy configuration.
-# Currently VERY limited - assumes Redhat/CentOS setup. Pull requests accepted!
+# Currently VERY limited - pull requests accepted!
 #
 # === Requirement/Dependencies:
 #
