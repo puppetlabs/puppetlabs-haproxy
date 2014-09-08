1ecf43d4d9207e09eb2d2b6eb7b69aa04370cf42
Jose Castro Leon <jose.castro.leon@cern.ch>
2014-09-08 13:46:27 +0200

    Removed unneeded parameters

--- manifests/frontend/acl.pp
+++ manifests/frontend/acl.pp
@@ -44,14 +44,12 @@
 #  haproxy::frontend::acl { 'puppet00-acl':
 #    frontend_name => 'puppet00',
 #    condition     => 'dst 127.0.0.1',
-#    acl_name      => 'acl_1',
 #    use_backend   => ''
 #  }
 
 define haproxy::frontend::acl (
   $frontend_name,
   $condition,
-  $acl_name       = $title,
   $use_backend    = ''
 ) {
 
@@ -60,8 +58,8 @@ define haproxy::frontend::acl (
   }
 
   # Template uses: $name, $ipaddress, $ports, $options
-  concat::fragment { "${frontend_name}_frontend_acl_${acl_name}_block":
-    order   => "15-${frontend_name}-01-${acl_name}",
+  concat::fragment { "${frontend_name}_frontend_acl_${name}_block":
+    order   => "15-${frontend_name}-01-${name}",
     target  => '/etc/haproxy/haproxy.cfg',
     content => template('haproxy/haproxy_frontend_acl.erb'),
   }
@@ -71,10 +69,10 @@ define haproxy::frontend::acl (
       fail ("No Haproxy::Backend[$use_backend] is defined!")
     }
 
-    haproxy::frontend::use_backend { "${use_backend}-${acl_name}":
+    haproxy::frontend::use_backend { "${use_backend}-${name}":
       frontend_name => $frontend_name,
       backend_name  => $use_backend,
-      if_acl        => $acl_name,
+      if_acl        => $name,
     }
   }
 }
