178745fd4a04a2a8bb7e51eb072376b03e5314ff
tlimoncelli@stackexchange.com <tlimoncelli@stackexchange.com>
2015-02-26 06:56:26 -0700

    Add haproxy::instance for the creation of multiple instances of haproxy on a host.  See [MODULES-1783]
      * New Define Resource Type: haproxy::instance
      * Class[haproxy] is now a wrapper that calls haproxy::instance
      * Add "instance" parameter to all public defines.
      * All private classes become private defined resource types.
      * Pass parameters to private defined resource types rather than
      * "inherts params"
      * Documentation updated.

--- manifests/peer.pp
+++ manifests/peer.pp
@@ -1,3 +1,4 @@
+#
 define haproxy::peer (
   $peers_name,
   $port,
@@ -6,11 +7,23 @@ define haproxy::peer (
   $ipaddresses  = $::ipaddress,
 ) {
 
-  # Templats uses $ipaddresses, $server_name, $ports, $option
-  concat::fragment { "peers-${peers_name}-${name}":
+  # We derive these settings so that the caller only has to specify $instance.
+  include haproxy::params
+  if $instance == 'haproxy' {
+    $instance_name = 'haproxy'
+    #$config_dir = $haproxy::params::config_dir
+    $config_file = $haproxy::params::config_file
+  } else {
+    $instance_name = "haproxy-${instance}"
+    #$config_dir = inline_template($haproxy::params::config_dir_tmpl)
+    $config_file = inline_template($haproxy::params::config_file_tmpl)
+  }
+
+  # Templates uses $ipaddresses, $server_name, $ports, $option
+  concat::fragment { "${instance_name}-peers-${peers_name}-${name}":
     order   => "30-peers-01-${peers_name}-${name}",
     ensure  => $ensure,
-    target  => '/etc/haproxy/haproxy.cfg',
+    target  => $config_file,
     content => template('haproxy/haproxy_peer.erb'),
   }
 }
