624726a4f5e3a5d939df3154c4fc281ae27755dc
daiana <daiana.mezdrea@yahoo.com>
2021-09-13 15:30:44 +0300

    Fix rubocop offenses

--- lib/facter/find_ip.rb
+++ lib/facter/find_ip.rb
@@ -1,10 +1,11 @@
 Puppet::Functions.create_function(:find_ip) do
-    def find_ip(hostname)
-        the_ip = nil
-        begin
-            the_ip = IPSocket.getaddress(hostname)
-        rescue SocketError
-        end
-        return the_ip
+  def find_ip(hostname)
+    the_ip = nil
+    begin
+      the_ip = IPSocket.getaddress(hostname)
+    rescue SocketError
+      puts 'SocketError'
     end
+    the_ip
+  end
 end
