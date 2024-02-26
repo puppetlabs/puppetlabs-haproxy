c4b714ea56b44606c2b3876d0158d955918cee9b
Tomas Barton <barton.tomas@gmail.com>
2024-02-26 09:45:08 +0100

    Update manifests/init.pp
    Co-authored-by: Tim Meusel <tim@bastelfreak.de>

--- manifests/init.pp
+++ manifests/init.pp
@@ -134,7 +134,7 @@ class haproxy (
   Hash                                          $global_options       = $haproxy::params::global_options,
   Hash                                          $defaults_options     = $haproxy::params::defaults_options,
   Boolean                                       $merge_options        = $haproxy::params::merge_options,
-  Optional[Array[String]]                       $install_options      = undef,
+  Array[String[1]]                              $install_options      = [],
   Optional[String]                              $restart_command      = undef,
   Optional[String]                              $custom_fragment      = undef,
   Stdlib::Absolutepath                          $config_dir           = $haproxy::params::config_dir,
